4bfe3355328b07b5ef90b6a5c7da5eed1e301bc0
Ryan Wilkinson <rwilk75@wgu.edu>
2022-10-09 10:33:59 -0400

    Add project files.

---
+++ comments/main.cpp
@@ -0,0 +1,16 @@
+/*
+* This is a comment. This is used for documentation and readability of code. Multi line is what this is.
+* 
+* 
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age = 30;  //this is my current age
+	double stateSalesTax = .06; // This is Michigans state sales tax.
+	cout << age << endl; cout<<  stateSalesTax << endl; 
+	return 0;
+
+}
